bff7096a2e4c85277d6c884dc4431e259d16a2a9
Purya Behdarvandan <purya.B97@gmail.com>
2023-02-28 23:52:05 +0300

    Markup & Styling

---
+++ anime-search/src/App.vue
@@ -0,0 +1,106 @@
+<script setup>
+import Card from "./components/Card.vue";
+
+const card = Card;
+</script>
+
+<template>
+  <div class="app">
+    <header>
+      <h1>The<strong>Anime</strong>Database</h1>
+
+      <form class="search-box">
+        <input
+          type="search"
+          class="search-field"
+          placeholder="Search for an anime..."
+        />
+      </form>
+    </header>
+    <main>
+      <div class="cards">
+        <Card />
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  font-family: "Fira Sans", sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  padding-top: 50px;
+  padding-bottom: 50px;
+}
+
+h1 {
+  color: #888;
+  font-size: 42px;
+  font-weight: 400;
+  text-align: center;
+  text-transform: uppercase;
+  margin-bottom: 30px;
+}
+
+h1:hover {
+  color: #313131;
+}
+
+strong {
+  color: #313131;
+}
+
+.search-box {
+  display: flex;
+  justify-content: center;
+  padding-left: 30px;
+  padding-right: 30px;
+}
+
+.search-field {
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  background-color: #f3f3f3;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+  display: block;
+  width: 100%;
+  max-width: 600px;
+  padding: 15px;
+  border-radius: 8px;
+  color: #313131;
+  font-size: 20px;
+
+  transition: 0.4s;
+}
+
+.search-field:focus {
+  color: #fff;
+  background-color: #313131;
+  box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.15);
+}
+
+main {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-left: 30px;
+  padding-right: 30px;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 -8px;
+}
+</style>
---
+++ anime-search/src/components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card">
+    <a href="" target="_blank">
+      <img src="https://placehold.it/150x250" alt="Anime Poster Image" />
+      <h3>Naruto</h3>
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  flex: 1 1 33%;
+  width: 33%;
+  max-width: 33%;
+  padding: 0px 8px;
+  margin-bottom: 16px;
+}
+
+a {
+  text-decoration: none;
+}
+
+img {
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+  border-radius: 16px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+  transition: 0.4s;
+}
+
+h3 {
+  padding: 16px;
+  color: #313131;
+  transition: 0.4s;
+}
+
+img:hover {
+  transform: scale(1.05);
+}
+
+h3:hover {
+  color: #24c444;
+}
+</style>
